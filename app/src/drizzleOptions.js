97763fdb79454ad7611afb91e50ab9c98132ccbc
baiyuqi <1353813001@qq.com>
2019-10-23 13:18:50 +0800

    dd

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,5 +1,6 @@
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import ComplexStorage from "./contracts/ComplexStorage.json";
+import registry from "./contracts/ComplianceServiceRegistry.json";
+import configurable from "./contracts/ConfigurableComplianceService.json";
+import configuration from "./contracts/ComplianceConfiguration.json";
 import TutorialToken from "./contracts/TutorialToken.json";
 
 const options = {
@@ -10,7 +11,7 @@ const options = {
       url: "ws://127.0.0.1:8545",
     },
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [registry, configurable, configuration],
   events: {
     SimpleStorage: ["StorageSet"],
   },
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,12 @@ const ComplianceServiceRegistry = artifacts.require("ComplianceServiceRegistry")
 const TutorialToken = artifacts.require("TutorialToken");
 const ConfigurableComplianceService = artifacts.require("ConfigurableComplianceService");
 
-module.exports = function(deployer) {
-  deployer.deploy(ComplianceServiceRegistry);
-  deployer.deploy(ConfigurableComplianceService);
+module.exports = async function(deployer) {
+
+  deployer.deploy(ComplianceServiceRegistry),
+  deployer.deploy(ConfigurableComplianceService),
   deployer.deploy(TutorialToken);
-};
+  //s1.setDefaultService(s2.address);
+ 
+
+}
