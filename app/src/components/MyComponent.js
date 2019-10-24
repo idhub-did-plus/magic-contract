0cc27b8e6484d83e4f33b660121e4ff98827a472
baiyuqi <1353813001@qq.com>
2019-10-24 12:35:18 +0800

    ss

--- app/src/components/MyComponent.js
+++ app/src/components/MyComponent.js
@@ -6,95 +6,95 @@ import {
 } from "@drizzle/react-components";
 
 
-export default class MyComponent extends Component{
+export default class MyComponent extends Component {
   constructor() {
     super();
     this.state = {
-        title: ""
+      title: ""
     };
-    
+
     this.handleChoose = this.handleChoose.bind(this);
-}
-handleChoose(event) {
+  }
+  handleChoose(event) {
     event.preventDefault();
     let index = parseInt(event.target.id);
     this.props.selectMenu(index);
-   
-}
+
+  }
   render() {
     return (
-  <div className="App">
-    
-    <div >
-      <ul class="topmenu">
-        <li><a href="#home" class="active">主页</a></li>
-        <li><a href="#news">新闻</a></li>
-        <li><a href="#contact">联系我们</a></li>
-        <li><a href="#about">关于我们</a></li>
-        <p>Magic Circle Compliace System</p>
-        
-      </ul>
-    </div>
-    <div class="column sidemenu">
-        <ul>
-         
-          <li ><p  id="0" onClick={this.handleChoose}  class="active">Compliance Service Registry</p></li>
-          <li ><p  id="1" onClick={this.handleChoose}  class="active">Compliance Configuration</p></li>
-          <li ><p  id="2" onClick={this.handleChoose}  class="active">Configurable Compliance Service</p></li>
+      <div className="App">
 
-        </ul>
-    </div>
-    <div>
-   
+        <div >
+          <ul class="topmenu">
+            <li><a href="#home" class="active">主页</a></li>
+            <li><a href="#news">新闻</a></li>
+            <li><a href="#contact">联系我们</a></li>
+            <li><a href="#about">关于我们</a></li>
+            <p>Magic Circle Compliace System</p>
+
+          </ul>
+        </div>
+        <div class="column sidemenu">
+          <ul>
+
+            <li ><p id="0" onClick={this.handleChoose} class="active">Compliance Service Registry</p></li>
+            <li ><p id="1" onClick={this.handleChoose} class="active">Compliance Configuration</p></li>
+            <li ><p id="2" onClick={this.handleChoose} class="active">Configurable Compliance Service</p></li>
+
+          </ul>
+        </div>
+        <div>
 
 
-    <div className="section" hidden = {this.props.currentMenu != 0}>
-      <h2>Compliance Service Registry</h2>
-      <p>
-        A  central place for tokens to  appoint their special compliance check logic and if not,  use a default one.
-      </p>
-      <p>
-        <strong>DefaultService: </strong>
-        <ContractData contract="ComplianceServiceRegistry" method="getDefaultService"  />
-      </p>
-      <h2>setDefaultService</h2>
-      <ContractForm contract="ComplianceServiceRegistry" method="setDefaultService" labels={["service address"]}  />
-      <h2>register</h2>
-      <ContractForm contract="ComplianceServiceRegistry" method="register" labels={["token address", "compliance service address"]}  />
-    </div>
 
-    <div className="section" hidden = {this.props.currentMenu != 1}>
-      <h2>ComplianceConfiguration</h2>
-      <p>
-        A place to configure the claim logic for token which use ConfigurableComplianceService
-      </p>
+          <div className="section" hidden={this.props.currentMenu != 0}>
+            <h2>Compliance Service Registry</h2>
+            <p>
+              A  central place for tokens to  appoint their special compliance check logic and if not,  use a default one.
+          </p>
+            <p>
+              <strong>DefaultService: </strong>
+              <ContractData contract="ComplianceServiceRegistry" method="getDefaultService" />
+            </p>
+            <h2>setDefaultService</h2>
+            <ContractForm contract="ComplianceServiceRegistry" method="setDefaultService" labels={["service address"]} />
+            <h2>register</h2>
+            <ContractForm contract="ComplianceServiceRegistry" method="register" labels={["token address", "compliance service address"]} />
+          </div>
 
-        <strong>getConfiguration: </strong>
-        <ContractForm
-          contract="ComplianceConfiguration"
-          method="getConfiguration"
-          labels={["token address"]}
-        />
-    
+          <div className="section" hidden={this.props.currentMenu != 1}>
+            <h2>ComplianceConfiguration</h2>
+            <p>
+              A place to configure the claim logic for token which use ConfigurableComplianceService
+            </p>
 
-      <h3>Send Tokens</h3>
-      <ContractForm
-        contract="ComplianceConfiguration"
-        method="setConfiguration"
-        labels={["token address", "configuarion"]}
-      />
-    </div>
-    <div className="section" hidden = {this.props.currentMenu != 2}>
-      <h2>Configurable Compliance Service</h2>
-      <p>
-      
-      </p>
-      <p>
-        This contract is the current default service of the compliance service registry. 
-      </p>
+            <strong>getConfiguration: </strong>
+            <ContractForm
+              contract="ComplianceConfiguration"
+              method="getConfiguration"
+              labels={["token address"]}
+            />
+
+
+            <h3>Send Tokens</h3>
+            <ContractForm
+              contract="ComplianceConfiguration"
+              method="setConfiguration"
+              labels={["token address", "configuarion"]}
+            />
+          </div>
+          <div className="section" hidden={this.props.currentMenu != 2}>
+            <h2>Configurable Compliance Service</h2>
+            <p>
+
+            </p>
+            <p>
+              This contract is the current default service of the compliance service registry.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     )
   }
 }
