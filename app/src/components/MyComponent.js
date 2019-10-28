83059779987874eb4ef6125cc5b5b21c018a7891
baiyuqi <yuqi.bai8888@gmail.com>
2019-10-28 09:35:58 +0800

    dd

--- app/src/components/MyComponent.js
+++ app/src/components/MyComponent.js
@@ -5,6 +5,103 @@ import {
   ContractForm,
 } from "@drizzle/react-components";
 import PropTypes from 'prop-types'
+import { drizzleConnect } from "@drizzle/react-plugin";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+export default function MyComponent() {
+  return (
+    <Router>
+      <div>
+        <ul  className="topmenu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    accounts: state.accounts,
+    ComplianceServiceRegistry: state.contracts.ComplianceServiceRegistry,
+    ConfigurableComplianceService: state.contracts.ConfigurableComplianceService,
+    ComplianceConfiguration: state.contracts.ComplianceConfiguration,
+    TutorialToken: state.contracts.TutorialToken,
+    drizzleStatus: state.drizzleStatus,
+    currentMenu: state.currentMenu,
+  };
+};
+
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
 
 class SideMenu extends Component {
   constructor() {
@@ -35,7 +132,7 @@ class SideMenu extends Component {
     )
   }
 }
-export default class MyComponent extends Component {
+  class MyComponentInternal extends Component {
   constructor() {
     super();
     
@@ -48,10 +145,7 @@ export default class MyComponent extends Component {
 
         <div >
           <ul className="topmenu">
-            <li><a href="#home" className="active">主页</a></li>
-            <li><a href="#news">新闻</a></li>
-            <li><a href="#contact">联系我们</a></li>
-            <li><a href="#about">关于我们</a></li>
+
             <p>Magic Circle Compliace System</p>
 
           </ul>
@@ -112,3 +206,7 @@ export default class MyComponent extends Component {
 MyComponent.contextTypes = {
   drizzleStore: PropTypes.object
 };
+const DoIt = drizzleConnect(MyComponentInternal, mapStateToProps);
+function Home() {
+  return <DoIt />
+}
