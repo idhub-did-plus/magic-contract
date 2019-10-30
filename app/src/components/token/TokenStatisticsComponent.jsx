3211557f009050a9fdb5f39fab84b4b7d6c6dbc3
baiyuqi <yuqi.bai8888@gmail.com>
2019-10-30 22:54:01 +0800

    dd

--- app/src/components/token/TokenStatisticsComponent.jsx
+++ app/src/components/token/TokenStatisticsComponent.jsx
@@ -8,13 +8,70 @@ import { DrizzleContext } from "@drizzle/react-plugin";
 
 export default function TokenStatisticsComponent(props) {
 
-  return (
-    <div className="mysection">
-      <h2>ComplianceConfiguration: </h2>
-      <p>
-        A place to configure the claim logic for token which use ConfigurableComplianceService
-</p>
-
-    </div>
-  )
+    return (
+        <Router>
+     <section id="wrap">
+          <nav className="sidemenu">
+            <ul >
+              <li>
+                <NavLink to="/erc20stat" activeClassName="active">Erc20 Token Statistics</NavLink>
+              </li>
+              <li>
+                <NavLink to="/erc1400stat" activeClassName="active">Erc1400 Token Statistics</NavLink>
+              </li>
+    
+              <li>
+                <NavLink to="/erc777stat" activeClassName="active">Erc777 Token Statistics</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <article className="wrapper">
+            <Switch>
+              <Route path="/erc20stat">
+                <Erc20StatComponent {...props} />
+              </Route>
+              <Route path="/erc1400stat">
+                <Erc1400StatComponent {...props} />
+              </Route>
+    
+              <Route path="/erc777stat">
+                <Erc777StatComponent {...props} />
+              </Route>
+            </Switch>
+          </article>
+          </section>
+    
+    
+        </Router>
+      )
+    function Erc20StatComponent(){
+        return (
+            <div className="mysection">
+              <h2>Erc20: </h2>
+              <p>
+                A place to statstics erc1400 security token
+              </p>
+            </div>
+          )
+    }
+    function Erc777StatComponent(){
+        return (
+            <div className="mysection">
+              <h2>Erc777: </h2>
+              <p>
+                A place to statstics erc777 security token
+              </p>
+            </div>
+          )
+    }
+    function Erc1400StatComponent(){
+        return (
+            <div className="mysection">
+              <h2>Erc1400: </h2>
+              <p>
+                A place to statstics erc1400 security token
+              </p>
+            </div>
+          )
+    }
 }
