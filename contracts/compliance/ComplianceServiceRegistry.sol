5653e7a762784594422564713922aea6f730a070
piouslove <305053045@qq.com>
2019-11-27 16:59:11 +0800

    revise code style

--- contracts/compliance/ComplianceServiceRegistry.sol
+++ contracts/compliance/ComplianceServiceRegistry.sol
@@ -5,25 +5,22 @@ contract ComplianceServiceRegistry {
     address defaultService;
     address owner;
     
-    constructor() public {
+    function ComplianceServiceRegistry() public {
         owner = msg.sender;
     }
+    
     function register(address token, address service) public {
-        require(msg.sender == owner, "only owner is valid!");
+        require(msg.sender == owner);
         services[token] = service;
     }
     
     function setDefaultService(address service) public {
-        //require(msg.sender == owner, "only owner is valid!");
+        require(msg.sender == owner);
         defaultService = service;
     }
     
     function getDefaultService() public view returns (address){
         return defaultService;
-    }
-      
-    function getOwner() public view returns (address){
-        return owner;
     }
     
     function findService(address token) public view returns (address){
