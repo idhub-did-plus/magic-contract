568a3e26ab4e403d7e6f6da1a3f890eae59bb87e
baiyuqi <1353813001@qq.com>
2019-12-02 11:49:09 +0800

    gg

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -1,5 +1,4 @@
-const ComplianceServiceRegistry = artifacts.require("ComplianceServiceRegistry");
-const ConfigurableComplianceService = artifacts.require("ConfigurableComplianceService");
+
 const ComplianceConfiguration = artifacts.require("ComplianceConfiguration");
 
 const EthereumClaimsRegistry = artifacts.require("EthereumClaimsRegistry");
@@ -7,11 +6,8 @@ const IdentityRegistry = artifacts.require("IdentityRegistry");
 const EthereumDIDRegistry = artifacts.require("EthereumDIDRegistry");
 const ERC1056 = artifacts.require("ERC1056");
 
-const Strings = artifacts.require("Strings");
-// const TutorialToken = artifacts.require("TutorialToken");
 
 module.exports = function (deployer) {
   deployer.deploy(ERC1056, IdentityRegistry.address, EthereumDIDRegistry.address);
-  deployer.deploy(ComplianceConfiguration);
   deployer.deploy(EthereumClaimsRegistry);
 }
