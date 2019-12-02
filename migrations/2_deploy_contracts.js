29833ba7d2da58157a058394ae33b36478bbe3fd
baiyuqi <1353813001@qq.com>
2019-12-02 11:43:24 +0800

    gg

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,7 @@
-const ComplianceServiceRegistry = artifacts.require("ComplianceServiceRegistry");
-const ConfigurableComplianceService = artifacts.require("ConfigurableComplianceService");
-const ComplianceConfiguration = artifacts.require("ComplianceConfiguration");
 
-const EthereumClaimsRegistry = artifacts.require("EthereumClaimsRegistry");
 const IdentityRegistry = artifacts.require("IdentityRegistry");
 const EthereumDIDRegistry = artifacts.require("EthereumDIDRegistry");
-const ERC1056 = artifacts.require("ERC1056");
 
-const Strings = artifacts.require("Strings");
-// const TutorialToken = artifacts.require("TutorialToken");
 module.exports = function (deployer) {
   deployer.deploy(IdentityRegistry);
   deployer.deploy(EthereumDIDRegistry);
--- migrations/4_deploy_contracts.js
+++ migrations/4_deploy_contracts.js
@@ -4,15 +4,12 @@ const ComplianceConfiguration = artifacts.require("ComplianceConfiguration");
 
 const EthereumClaimsRegistry = artifacts.require("EthereumClaimsRegistry");
 const IdentityRegistry = artifacts.require("IdentityRegistry");
-const EthereumDIDRegistry = artifacts.require("EthereumDIDRegistry");
 const ERC1056 = artifacts.require("ERC1056");
-
 const Strings = artifacts.require("Strings");
-// const TutorialToken = artifacts.require("TutorialToken");
+
 
 module.exports = function (deployer) {
     deployer.deploy(ConfigurableComplianceService,
-    ComplianceConfiguration.address,
     EthereumClaimsRegistry.address,
     IdentityRegistry.address,
     ERC1056.address);
@@ -21,5 +18,4 @@ module.exports = function (deployer) {
     deployer.link(Strings, ConfigurableComplianceService);
 
     deployer.deploy(ComplianceServiceRegistry);
-    deployer.deploy(TutorialToken);
 }
