c150f9acf1f26f88bf3d8ddb4034cdd666620cea
baiyuqi <1353813001@qq.com>
2019-12-02 13:04:46 +0800

    gg

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -9,5 +9,6 @@ const ERC1056 = artifacts.require("ERC1056");
 
 module.exports = function (deployer) {
   deployer.deploy(ERC1056, IdentityRegistry.address, EthereumDIDRegistry.address);
+  deployer.deploy(ComplianceConfiguration);
   deployer.deploy(EthereumClaimsRegistry);
 }
