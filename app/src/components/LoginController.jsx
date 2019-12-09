99c409e6a4270001df38252d014cf7ea37a1f23f
baiyuqi <1353813001@qq.com>
2019-12-09 11:43:12 +0800

    dd

--- app/src/components/LoginController.jsx
+++ app/src/components/LoginController.jsx
@@ -32,18 +32,38 @@ export default class LoginController extends Component {
     }
     var myDate = new Date();
 		var timestamp = myDate.getTime();
-    var data = web3.fromUtf8(timestamp + web3.eth.coinbase)
+    var data = web3.fromUtf8(web3.eth.coinbase + timestamp + claim)
+    let web3 = this.props.drizzle.web3;
     web3.personal.sign(data, web3.eth.coinbase,(error, signature)=>{
       var identity = web3.eth.coinbase;
-      ajax.open('get','auth/login?identity='+identity+'&timestamp='+timestamp + "&signature=" + signature);
-      ajax.send(null);
+      request(identity, timestamp, this.claim, signature);
+    
     });
 
-
-    
-
-
   }
+  async request(identity, tp,claim, sig) {
+    try {
+      let response = await fetch('http://localhost:8080/login?identity='+identity+'&timestamp='+tp + '&claim=' + claim+ '&signature=' + sig, {
+       
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, same-origin, *omit
+        headers: {
+          'user-agent': 'Mozilla/4.0 MDN Example',
+          'content-type': 'application/json'
+        },
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        redirect: 'follow', // manual, *follow, error
+        referrer: 'no-referrer', // *client, no-referrer
+      })
+      let json = response.json() // parses response to JSON
+    } catch (err) {
+      alert(err);
+    } finally {
+
+    }
+  }
+
   render() {
     let login = this.props.drizzle.store.getState().login;
     if (login === true) {
