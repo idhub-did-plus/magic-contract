eb2ed107f8b96cf6680ab2247225a6ce9561ee42
baiyuqi <1353813001@qq.com>
2019-12-09 10:48:09 +0800

    dd

--- app/src/components/LoginController.jsx
+++ app/src/components/LoginController.jsx
@@ -2,31 +2,63 @@
 
 import React, { Component, Children } from "react";
 import { DrizzleContext } from "@drizzle/react-plugin";
-import {login} from "../store/login/actions"
+import { login } from "../store/login/actions"
+import { Dropdown, Button, Checkbox, Table, TableBody, TableCell, TableRow } from 'semantic-ui-react'
 
- export default class LoginController extends Component {
-    constructor(props, context) {
-      super(props)
-    }
-    componentDidMount(){
-      this.props.drizzle.store.dispatch(login());
 
-    }
-    render() {
-    let login = this.props.drizzle.store.getState().login;
-      if (login === true)
-      {
-        // Load the dapp.
-        return Children.only(this.props.children)
-      }
-  
-      return(
-        // Display a loading indicator.
-        <main>
-          <h1>⚙️</h1>
-          <p>Login dapp...</p>
-        </main>
-      )
-    }
+
+export default class LoginController extends Component {
+  constructor(props, context) {
+    super(props)
+
+    this.handleClaimChange = this.handleClaimChange.bind(this);
+    this.login = this.login.bind(this);
+
   }
- 
+  componentDidMount() {
+
+    //this.props.drizzle.store.dispatch(login());
+
+  }
+  handleClaimChange(key, text, value) {
+
+    this.claim = text.value;
+
+  } 
+  login(event){
+
+  }
+  render() {
+    let login = this.props.drizzle.store.getState().login;
+    if (login === true) {
+      // Load the dapp.
+      return Children.only(this.props.children)
+    }
+
+    return (
+      // Display a loading indicator.
+      <main>
+        <h1>⚙️</h1>
+        <p>Login dapp...</p>
+        <Dropdown
+          placeholder='Select Country'
+
+          search
+          selection
+          options={countryOptions}
+          onChange={this.handleClaimChange}
+        />
+        <Button 
+        onClick={this.login}
+        />
+      </main>
+    )
+  }
+}
+
+const countryOptions = [
+  { key: 'af', value: 'af', text: 'Afghanistan' },
+  { key: 'ax', value: 'ax', text: 'Aland Islands' },
+  { key: 'al', value: 'al', text: 'Albania' },
+]
+
