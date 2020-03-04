22e3bb4c9446a98566915cbf3d50ff9cbb630fe0
piouslove <305053045@qq.com>
2020-03-04 13:47:31 +0800

    init security contract

---
+++ contracts/security/SecurityToken.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.5.8;
+
+import "./proxy/OwnedUpgradeabilityProxy.sol";
+// import "./UpgradeabilityProxy.sol";
+import "./SecurityTokenStorage.sol";
+
+/**
+ * @title SecurityTokenProxy SecurityToken
+ */
+contract SecurityToken is SecurityTokenStore, OwnedUpgradeabilityProxy {
+
+    /**
+    * @notice Constructor
+    * @param _implementation representing the address of the new implementation to be set
+    */
+    constructor(address _implementation) public {
+        require(_implementation != address(0), "Address should not be 0x");
+        // securityToken = ISecurityToken(_securityToken);
+        __implementation = _implementation;
+    }
+
+    /**
+    * @notice Internal function to provide the address of the implementation contract
+    */
+    function _implementation() internal view returns(address) {
+        return __implementation;
+    }
+
+}
