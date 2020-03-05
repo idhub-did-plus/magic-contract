65ec8fd8f5cbdbf026f13ad5b873f61c3c04b9e1
piouslove <305053045@qq.com>
2020-03-05 12:47:46 +0800

    add ERC1643 fuctions

--- contracts/security/SecurityTokenStorage.sol
+++ contracts/security/SecurityTokenStorage.sol
@@ -63,6 +63,26 @@ contract SecurityTokenStorage {
 
     // permissions[contract-address][from-address][to-address][perm-name] => bool
 
+
+    //////////////////////////
+    /// Document datastructure
+    //////////////////////////
+    
+    // mapping to store the documents details in the document
+    mapping(bytes32 => Document) internal _documents;
+    // mapping to store the document name indexes
+    mapping(bytes32 => uint256) internal _docIndexes;
+
+    // Array use to store all the document name present in the contracts
+    bytes32[] _docNames;
+
+    struct Document {
+        bytes32 docHash; // Hash of the document
+        uint256 lastModified; // Timestamp at which document details was last modified
+        string uri; // URI of the document that exist off-chain
+    }
+
+    
     //////////////////////////
     /// Module datastructure
     //////////////////////////
--- contracts/security/SecurityTokenStore.sol
+++ contracts/security/SecurityTokenStore.sol
@@ -8,6 +8,20 @@ contract SecurityTokenStore is SecurityTokenStorage, DataStore  {
         require(msg.sender == _owner, "Unauthorized");
     }
     // data getter
+
+    //////////////////////////
+    /// Document Fuctions
+    //////////////////////////
+
+    function getDocNames() external view returns(bytes32[] memory) {
+        return _docNames;
+    }
+
+    function getDocument(bytes32 _name) external view returns (string memory, bytes32, uint256) {
+        return (_documents[_name].uri, _documents[_name].docHash, _documents[_name].lastModified);
+    }
+
+
     function getName(address _partition) external view returns(string memory) {
         return names[_partition];
     }
@@ -33,6 +47,83 @@ contract SecurityTokenStore is SecurityTokenStorage, DataStore  {
     }
 
     // data setter
+
+    //////////////////////////
+    /// Document Fuctions
+    //////////////////////////
+
+    /**
+     * @notice Used to attach a new document to the contract, or update the URI or hash of an existing attached document
+     * @dev Can only be executed by the owner of the contract.
+     * @param _name Name of the document. It should be unique always
+     * @param _uri Off-chain uri of the document from where it is accessible to investors/advisors to read.
+     * @param _documentHash hash (of the contents) of the document.
+     */
+    function setDocument(bytes32 _name, string calldata _uri, bytes32 _documentHash) external {
+        _isAuthorized();
+        _setDocument(_documents, _docNames, _docIndexes, _name, _uri, _documentHash);
+    }
+
+    /**
+     * @notice Used to remove an existing document from the contract by giving the name of the document.
+     * @dev Can only be executed by the owner of the contract.
+     * @param _name Name of the document. It should be unique always
+     */
+    function removeDocument(bytes32 _name) external {
+        _isAuthorized();
+        _removeDocument(_documents, _docNames, _docIndexes, _name);
+    }
+
+    /**
+     * @notice Used to attach a new document to the contract, or update the URI or hash of an existing attached document
+     * @param name Name of the document. It should be unique always
+     * @param uri Off-chain uri of the document from where it is accessible to investors/advisors to read.
+     * @param documentHash hash (of the contents) of the document.
+     */
+    function _setDocument(
+        mapping(bytes32 => Document) storage document,
+        bytes32[] storage docNames,
+        mapping(bytes32 => uint256) storage docIndexes,
+        bytes32 name,
+        string memory uri,
+        bytes32 documentHash
+    )
+        internal
+    {
+        if (document[name].lastModified == uint256(0)) {
+            // check if exists bug? 
+            // index start from 1
+            docNames.push(name);
+            docIndexes[name] = docNames.length;
+            // docNames.push(name);
+        }
+        document[name] = Document(documentHash, now, uri);
+    }
+
+    /**
+     * @notice Used to remove an existing document from the contract by giving the name of the document.
+     * @dev Can only be executed by the owner of the contract.
+     * @param name Name of the document. It should be unique always
+     */
+    function _removeDocument(
+        mapping(bytes32 => Document) storage document,
+        bytes32[] storage docNames,
+        mapping(bytes32 => uint256) storage docIndexes,
+        bytes32 name
+    )
+        internal
+    {
+        require(document[name].lastModified != uint256(0), "Not existed");
+        uint256 index = docIndexes[name] - 1;
+        if (index != docNames.length - 1) {
+            docNames[index] = docNames[docNames.length - 1];
+            docIndexes[docNames[index]] = index + 1;
+        }
+        docNames.length--;
+        delete document[name];
+    }
+
+
     function setName(address _partition, string calldata _name) external {
         _isAuthorized();
         names[_partition] = _name;
